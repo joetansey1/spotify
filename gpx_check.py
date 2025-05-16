aebcb0e7dffdbb4f182d0b730b84813cde94fe16
joetansey1 <70726791+joetansey1@users.noreply.github.com>
2025-05-15 17:16:04 -0700

    Delete gpx_check.py

--- gpx_check.py
+++
@@ -1,9 +0,0 @@
-import gpxpy
-
-gpx_file = "/home/joetanse/strava_data/activities/138722454.gpx"
-try:
-    with open(gpx_file, 'r') as file:
-        gpx = gpxpy.parse(file)
-    print(f"Parsed {gpx_file} successfully")
-except Exception as e:
-    print(f"Error parsing {gpx_file}: {e}")
